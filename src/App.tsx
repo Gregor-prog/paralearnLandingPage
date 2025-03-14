cf118ef97a04023774e738dd97543a467f81334d
Greg-prog <olajideemmanuel250@gmail.com>
2025-03-14 09:28:40 +0000

    fixed_space

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import LandingPage from "./pages/landingPage"
 function App(){
-  return <div className="w-[100dvw]">
+  return <div className="w-[100dvw] overflow-x-hidden">
     <LandingPage/>
   </div>
 }
