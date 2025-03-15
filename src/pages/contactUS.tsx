2c469767fc63a1aef5eaf0448fe55d460b40c3d9
Greg-prog <olajideemmanuel250@gmail.com>
2025-03-15 19:51:53 +0000

    emailMessage

--- src/pages/contactUS.tsx
+++ src/pages/contactUS.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState,useRef } from "react"
 import map from "../assets/Screenshot (10) 1.png"
 import location from "../assets/ic_baseline-location-on.png"
 import email from "../assets/dashicons_email-alt.png"
@@ -10,10 +10,11 @@ import EmailJS from "@emailjs/browser"
 function Contact(){
         const [Username, setUsername] = useState("")
         const [text,settext] = useState("")
+        const [Email,setEmail] = useState("")
+        const form = useRef< HTMLFormElement>(null)
 
         async function submit(e:any){
             e.preventDefault()
-            
             try {
                 if(Username == "" || Username == null){
                     throw new Error("username not found");
@@ -21,22 +22,26 @@ function Contact(){
                 if(text == "" || text == null){
                     throw new Error("feedback field is empty");
                 }
-                // const send = await fetch("https://script.google.com/macros/s/AKfycbwgYOFV0e8FzqbaYjBMUs8NugLhAERzZzaW3clsSB4wzsUe6i9nwFKJB_gUvAtCjuvelw/exec", {
-                //     method: 'POST',
-                //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-                //     body:(`username=${Username}&feedback=${text}`)
-                // })
-                EmailJS.sendForm("serviceid","template", "")
-                // if(!send.ok){throw new Error("could'nt send feedback");}
-                toast.success("Thanks for the feedback")
+                if(Email == "" || Email == null){
+                    throw new Error("Email field is empty");
+                }
+
+                
+                EmailJS.sendForm("service_1l4b0o8","template_db30gzb", form.current!, "K7GEK8OM6BBFQ0Idl")
+                .then(() => toast.success("Thanks for the feedback"))
+                .catch(() =>  toast.error("couldn't send feedback") )
+                
                 setUsername("")
                 settext("")
             } catch (error) {
                 if(error){
-                    console.log(error)
                     toast.error("couldn't send feedback")
                 }
+                
             }
+                
+            
+            
 
         }
     return <div className="flex flex-col items-center">
@@ -53,10 +58,11 @@ function Contact(){
                 </div>
             </div>
 
-            <form onSubmit={(e) => submit(e)} className=" sm:w-[50%] flex flex-col items-center ">
+            <form onSubmit={(e) => submit(e)} ref={form} className=" sm:w-[50%] flex flex-col items-center ">
             <h1 className="font-bold text-[45px] text-[#272643] my-[40px]">drop your Feedback</h1>
-            <input type="text" value={Username} onChange={(e) => {setUsername(e.target.value)}} placeholder="Username" className="sm:w-[503px] w-[300px] h-[62px] p-[10px] border-[1px] border-[#F53838] mb-[20px]"/>
-            <textarea value={text} onChange={(e) => {settext(e.target.value)}} className="h-[303px] sm:w-[503px] w-[300px] border-[1px] border-[#272643] mt-[30px] p-[20px]" placeholder="your FeedBack..."/>
+            <input type="text" name="name" value={Username} onChange={(e) => {setUsername(e.target.value)}} placeholder="Username" className="sm:w-[503px] w-[300px] h-[62px] p-[10px] border-[1px] border-[#F53838] mb-[20px]"/>
+            <input type="Email" name="Email" value={Email} onChange={(e) => {setEmail(e.target.value)}} placeholder="Email" className="sm:w-[503px] w-[300px] h-[62px] p-[10px] border-[1px] border-[#F53838] mb-[20px]"/>
+            <textarea value={text} name="message" onChange={(e) => {settext(e.target.value)}} className="h-[303px] sm:w-[503px] w-[300px] border-[1px] border-[#272643] mt-[30px] p-[20px]" placeholder="your FeedBack..."/>
 
             <button  className="sm:w-[503px] w-[300px] h-[52px] bg-[#F53838] rounded-[6px] text-[16px] font-semibold text-white my-[50px]">Message</button>
 
